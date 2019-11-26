df6c8783e0d6b2f5876bf73acdda298c8ee46472
pshibu777 <pshibu777@gmail.com>
2019-11-26 13:07:26 +0530

    Flask

--- application.py
+++ application.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, world!"
+
+@app.route("/shibu")
+def shibu():
+    return "Hello, shibu!"
+
+@app.route("/arun")
+def arun():
+    return "Hello, arun!"
