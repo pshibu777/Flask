a37c828a9da6d2219dc1a4247caeb0ba7477bebd
pshibu777 <pshibu777@gmail.com>
2019-12-03 12:14:37 +0530

    flask_app

--- flask_app/inheritance/inheritance.py
+++ flask_app/inheritance/inheritance.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('firstpage.html')
+    return render_template("firstpage.html")
 
+@app.route("/more")
 def more():
-    return render_template('more.html')
+    return render_template("more.html")
